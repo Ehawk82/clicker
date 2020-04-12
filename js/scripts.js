c13db0bbf5d635d104844dbc8ebe8b510f31d886
Jason Graziano <mrehawk82@gmail.com>
2020-04-12 12:11:05 -0400

    UI setup

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,28 @@ var myUI = {
 			container = createEle("div")
 			footer = createEle("footer");
 
-		header.innerHTML = "CLICKER GAME";
+		header.innerHTML = "<span>SEABEE CLICKER</span>";
 
-		container.innerHTML = "body";
 		container.className = "container";
+		container.onload = myUI.loadout(header,container);
 
 		footer.innerHTML = "EHAWK &copy; 2020";
 
 		body.append(header,container,footer);
+	},
+	loadout: function(header,container){
+		var tools = createEle("button");
+
+		tools.innerHTML = "⚙️";
+		tools.className = "tools";
+
+		header.append(tools);
+		for (var i = 0; i < 3; i++) {
+			var holders = createEle("div");
+			holders.className = "holders";
+			holders.innerHTML = "holder " + i;
+			container.append(holders);
+		}
 	}
 };
 window.onload = function(){
