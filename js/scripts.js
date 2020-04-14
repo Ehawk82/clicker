d6cae7508480e5707e0152c8f431d99f6d4f1209
Jason Graziano <mrehawk82@gmail.com>
2020-04-14 14:09:17 -0400

    UI design and setting up for responsive

--- js/scripts.js
+++ js/scripts.js
@@ -7,24 +7,86 @@ var myUI = {
 		header.innerHTML = "<span>SEABEE CLICKER</span>";
 
 		container.className = "container";
-		container.onload = myUI.loadout(header,container);
+		container.onload = myUI.loadout(header,container,footer);
 
-		footer.innerHTML = "EHAWK &copy; 2020";
+		//footer.innerHTML = "EHAWK &copy; 2020";
 
 		body.append(header,container,footer);
 	},
-	loadout: function(header,container){
-		var tools = createEle("button");
+	loadout: function(header,container,footer){
+		var tools = createEle("button"),
+		    achievements = createEle("button"),
+		    stats = createEle("button"),
+		    funds = createEle("div"),
+		    readiness = createEle("div"),
+		    skills = createEle("div");
 
 		tools.innerHTML = "⚙️";
 		tools.className = "tools";
+		tools.onclick = myUI.popup(tools);
 
-		header.append(tools);
+		achievements.innerHTML = "🏆";
+		achievements.className = "achievements";
+		achievements.onclick = myUI.popup(achievements);
+
+		stats.innerHTML = "📈";
+		stats.className = "stats";
+		stats.onclick = myUI.popup(stats);
+
+		header.append(tools,achievements,stats);
+
 		for (var i = 0; i < 3; i++) {
 			var holders = createEle("div");
 			holders.className = "holders";
-			holders.innerHTML = "holder " + i;
+			holders.innerHTML = "&nbsp;";
+
 			container.append(holders);
+		}
+		funds.innerHTML = "FUNDS";
+		funds.setAttribute("data-index", 0);		
+		funds.onmouseover = myUI.pophover(funds);
+
+		readiness.innerHTML = "READINESS";
+		readiness.setAttribute("data-index", 1);
+		readiness.onmouseover = myUI.pophover(readiness);
+
+		skills.innerHTML = "SKILLS";
+		skills.setAttribute("data-index", 2);
+		skills.onmouseover = myUI.pophover(skills);
+
+		footer.append(funds,readiness,skills);
+	},
+	pophover: function(x){
+		return function(){
+			var thing = x.innerHTML,
+				dindex = x.getAttribute('data-index');
+			x.innerHTML = basic[dindex];
+			x.onmouseout = function(){ return x.innerHTML = thing }
+		};
+	},
+	popup: function(x){
+		return function(){
+			var blokker = createEle("div"),
+				xOut = createEle("button"),
+				frame = createEle("div");
+
+			frame.innerHTML = x.innerHTML;
+			frame.className = "frame";
+
+			xOut.innerHTML = "❌";
+			xOut.className = "xOut";
+			xOut.onclick = myUI.xOutFunc(blokker);
+
+			blokker.innerHTML = "&nbsp;";
+			blokker.className = "blokker";
+			blokker.append(xOut,frame);
+
+			body.append(blokker);
+		}
+	},
+	xOutFunc: function(blokker){
+		return function(){
+			blokker.remove();
 		}
 	}
 };
